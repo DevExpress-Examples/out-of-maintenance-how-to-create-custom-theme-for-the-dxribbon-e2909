2db7284024cb72cdf7e6bdd4f47efe6300d7b3bf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create custom theme for the DXRibbon
+
+
+<p>This example illustrates how to override templates for the corresponding controls to change the DXRibbonControl appearance. The corresponding approach is described in detail in the following article: <a href="https://www.devexpress.com/Support/Center/p/ka18580">How to modify DX themes in WPF</a>.</p>
+
+<br/>
+
+
---
+++ VB/WpfApplication1/Application.xaml.vb
@@ -0,0 +1,26 @@
+﻿' Developer Express Code Central Example:
+' How to create custom theme for the DXRibbon
+' 
+' Override our themes' templates, and embed your templates to the
+' BarManager.Resources.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E2909
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+
+Namespace WpfApplication1
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+	End Class
+End Namespace
